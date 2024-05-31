f4edc744ccf56327ecea78844536df1d146ea007
Luka Šuto <lsuto22@foi.hr>
2024-05-31 20:53:10 +0200

    Revert "primjer"
    This reverts commit eaeaed42e4ee2b8b3a79c3d05bcb1f51fae0fc93.

--- MyDoc/MyDoc/FrmLogin.Designer.cs
+++ MyDoc/MyDoc/FrmLogin.Designer.cs
@@ -28,32 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(379, 192);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(143, 22);
-            this.textBox1.TabIndex = 0;
-            // 
-            // FrmLogin
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Name = "FrmLogin";
+            this.Name = "Form1";
             this.Text = "Login forma";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 

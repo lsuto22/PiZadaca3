bdee92eb5d6b283aa806d153903f3171f496c036
Luka Šuto <lsuto22@foi.hr>
2024-05-31 15:28:27 +0200

    Kreirana forma za logiranje i uspostavljen DLL

---
+++ MyDoc/MyDoc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDoc
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DBLayer.DB.SetConfiguration("PI2324_lsuto22_DB", "PI2324_lsuto22_User", "Z1/2euzs");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
